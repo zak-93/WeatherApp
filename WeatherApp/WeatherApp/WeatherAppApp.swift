b51279a0c9b791e65ed0dfca4e46f2d796757bbe
zahar.yashin <>
2023-05-11 12:22:56 +0700

    Create app

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yashin Zahar on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
