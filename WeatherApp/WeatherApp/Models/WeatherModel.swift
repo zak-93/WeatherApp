67d860c75fa4ccbd9fb6caf8a20ab2a6d0e7f107
zahar.yashin <>
2023-05-12 17:20:16 +0700

    Create model app

---
+++ WeatherApp/WeatherApp/Models/WeatherModel.swift
@@ -0,0 +1,190 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Yashin Zahar on 12.05.2023.
+//
+
+import Foundation
+
+struct WeatherCurrent: Codable {
+    let coord: Coord
+    let weather: [WeatherCurrentDay]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: WindCurrent
+    let clouds: Clouds
+    let dt: Int
+    let sys: SysCurrent
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon: Double
+    let lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let pressure: Int
+    let humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case humidity
+    }
+}
+
+// MARK: - Sys
+struct SysCurrent: Codable {
+    let type: Int
+    let id: Int
+    let country: String
+    let sunrise: Int
+    let sunset: Int
+}
+
+// MARK: - Weather
+struct WeatherCurrentDay: Hashable, Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+// MARK: - Wind
+struct WindCurrent: Codable {
+    let speed: Int
+    let deg: Int
+}
+
+// MARK: - ModalApp
+struct WeatherAllDay: Codable {
+    let cod: String
+    let message: Int
+    let cnt: Int
+    let list: [List]
+    let city: City
+}
+
+// MARK: - City
+struct City: Codable {
+    let id: Int
+    let name: String
+    let coord: Coord
+    let country: String
+    let population: Int
+    let timezone: Int
+    let sunrise: Int
+    let sunset: Int
+}
+
+// MARK: - List
+struct List: Identifiable, Codable {
+    let id = UUID().uuidString
+    let dt: Int
+    let main: MainClass
+    let weather: [Weather]
+    let clouds: Clouds
+    let wind: Wind
+    let visibility: Int
+    let pop: Double
+    let sys: Sys
+    let dtTxt: String
+    let rain: Rain?
+
+    enum CodingKeys: String, CodingKey {
+        case dt
+        case main
+        case weather
+        case clouds
+        case wind
+        case visibility
+        case pop
+        case sys
+        case dtTxt = "dt_txt"
+        case rain
+    }
+}
+
+// MARK: - MainClass
+struct MainClass: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let pressure: Int
+    let seaLevel: Int
+    let grndLevel: Int
+    let humidity: Int
+    let tempKf: Double
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
+    }
+}
+
+// MARK: - Rain
+struct Rain: Codable {
+    let the3H: Double
+
+    enum CodingKeys: String, CodingKey {
+        case the3H = "3h"
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let pod: Pod
+}
+
+enum Pod: String, Codable {
+    case d = "d"
+    case n = "n"
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main: MainEnum
+    let description: String
+    let icon: String
+}
+
+enum MainEnum: String, Codable {
+    case clear = "Clear"
+    case clouds = "Clouds"
+    case rain = "Rain"
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
